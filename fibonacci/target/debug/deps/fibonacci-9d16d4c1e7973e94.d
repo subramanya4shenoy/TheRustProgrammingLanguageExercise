d4fab7234f27440c03880648bfc05e5151f85c8a
Subu <subramanya4shenoy@gmail.com>
2023-12-28 14:21:11 +0530

    Delete fibonacci directory

--- fibonacci/target/debug/deps/fibonacci-9d16d4c1e7973e94.d
+++
@@ -1,5 +0,0 @@
-/Volumes/SubuSSD2/Rust Projects/Chapter3/fibonacci/target/debug/deps/libfibonacci-9d16d4c1e7973e94.rmeta: src/main.rs
-
-/Volumes/SubuSSD2/Rust Projects/Chapter3/fibonacci/target/debug/deps/fibonacci-9d16d4c1e7973e94.d: src/main.rs
-
-src/main.rs:
