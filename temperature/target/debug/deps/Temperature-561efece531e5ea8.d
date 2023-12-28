22ca5d39fcaca7d65599dea6870e443c60785532
Subu <subramanya4shenoy@gmail.com>
2023-12-28 14:21:30 +0530

    Delete temperature directory

--- temperature/target/debug/deps/Temperature-561efece531e5ea8.d
+++
@@ -1,5 +0,0 @@
-/Volumes/SubuSSD2/Rust Projects/Chapter3/temperature/target/debug/deps/libTemperature-561efece531e5ea8.rmeta: src/main.rs
-
-/Volumes/SubuSSD2/Rust Projects/Chapter3/temperature/target/debug/deps/Temperature-561efece531e5ea8.d: src/main.rs
-
-src/main.rs:
