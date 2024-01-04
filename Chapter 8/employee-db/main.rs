d400e3f90a26bc9352a8f6fc8d3765c3f2ea7bea
Subu <subramanya4shenoy@gmail.com>
2024-01-04 20:57:55 +0530

    more methods added to chapter 8 employeeDB

--- Chapter 8/employee-db/main.rs
+++ Chapter 8/employee-db/main.rs
@@ -1,15 +1,13 @@
 use std::io;
-
-enum UserOperations {
-    Add,
-    Remove
-}
+use str::collection::Hashmap;
 
 fn main() {
     // ask user if they want to add info or retrieve info?
     let mut user_choice: String = String::new();
     //collect user input for entry
     let mut user_cmd: String = String::new();
+
+    let mut db: Hashmap: Hashmap::new();
 
     println!("Hello!");
     println!("What do you want to do today?");
@@ -28,8 +26,9 @@ fn main() {
             io::stdin()
                 .read_line(&mut user_cmd)
                 .expect("Enter valid texts");
-            let (name, department) = extract_name_department(user_cmd.trim().to_string());
-            println!("{},{}", name, department);
+            let (name, department, operation) = extract_info(user_cmd.trim());
+            println!("{},{},{}", name, department, operation);
+            // update our db
         }
         _ => {
             println!("Enter a valid choice!");
@@ -38,6 +37,39 @@ fn main() {
 
 }
 
-fn extract_name_department(text: String) -> (String, String) {
-    (String::from("Subu"), String::from("CS"))
+// Function to extract info from string
+fn extract_info(text: &str) -> (String, String, String) {
+    // identify is it a add operation or remove operation
+    let operation: String = extract_operation(text);
+    // get the name, department out of the string
+    let(name , department) = extract_name_department(text);
+
+    (name, department, operation)
+}
+
+
+// function to extract operation from string
+fn extract_operation(text: &str) -> String {
+    
+    let mut operation: String;
+
+    if text.to_lowercase().starts_with("add") {
+        operation = String::from("ADD");
+    } else if text.to_lowercase().starts_with("remove") {
+        operation = String::from("REMOVE");
+    } else {
+        panic!("Error!!!")
+    } 
+    operation
+}
+
+// function to extract name and department from the string
+fn extract_name_department(text: &str) -> (String, String) {
+    let words_in_lowercase = text.to_lowercase();
+    let word: Vec<&str> = words_in_lowercase.split_whitespace().collect();
+
+    match word.as_slice() {
+        ["add", name @ .., "to", department ] | ["remove", name @ .., "from", department  ] =>( name.join(" ").to_string(), department.to_string()),
+        _ => (String::from("No valid name found"), String::from("No valid department found"))
+    }
 }
