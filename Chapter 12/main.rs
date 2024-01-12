87ee521c33d11a338c0335a468d5fb598f1edd3c
Subu <subramanya4shenoy@gmail.com>
2024-01-12 19:44:50 +0530

    chapter 12 init 2

--- Chapter 12/main.rs
+++ Chapter 12/main.rs
@@ -1,5 +1,29 @@
 use std::env;
+use std::fs;
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    dbg!("{:?}", args);
+   let args: Vec<String> = env::args().collect();
+
+   let config: Config = parse_config(&args);
+
+   println!("Sraching for the text {}", config.query);
+   println!("In the file {}", config.file_path);
+
+   let contents: String = fs::read_to_string(config.file_path).expect("Should be able to read the file");
+
+   println!("contents {}", contents);
+
+}
+
+struct Config {
+   query: String,
+   file_path: String
+}
+
+fn parse_config(args: &[String]) -> Config {
+   
+   let query: String = args[1].clone();
+   let file_path: String = args[2].clone();
+
+   Config { query, file_path }
 }
