2e888384f7b8feb11bdde63ef73d95c8f1a9962f
Subu <subramanya4shenoy@gmail.com>
2024-01-13 19:42:39 +0530

    chapter 12 sperating logic and rest of the program

--- Chapter 12/main.rs
+++ Chapter 12/main.rs
@@ -1,29 +1,44 @@
 use std::env;
 use std::fs;
-
+use std::process;
+use std::error::Error;
 fn main() {
-   let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
-   let config: Config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err: &str| {
+        println!("Problem parsing input: {}", err);
+        process::exit(1);
+    });
 
-   println!("Sraching for the text {}", config.query);
-   println!("In the file {}", config.file_path);
+    println!("Sraching for the text {}", config.query);
+    println!("In the file {}", config.file_path);
 
-   let contents: String = fs::read_to_string(config.file_path).expect("Should be able to read the file");
+    if let Err (e) = run(config) {
+        println!("App error: {e}");
+        process::exit(1);
+    };
+}
 
-   println!("contents {}", contents);
-
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents: String =
+    fs::read_to_string(config.file_path) ?;
+    println!("contents {}", contents);
+    Ok(())
 }
 
 struct Config {
-   query: String,
-   file_path: String
+    query: String,
+    file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-   
-   let query: String = args[1].clone();
-   let file_path: String = args[2].clone();
+impl Config {
+    fn build(args: &[String]) -> Result <Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Arguments are insuffiecient!!");
+        }
+        let query: String = args[1].clone();
+        let file_path: String = args[2].clone();
 
-   Config { query, file_path }
+        Ok(Config { query, file_path })
+    }
 }
